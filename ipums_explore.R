9254c9472956eb4f4f27a42935e8f3479ea0f445
Tal Roded <troded24@gmail.com>
2022-08-09 13:30:22 -0400

    Starting new project folder for beginning data analysis of Chicago historical data

---
+++ ipums_explore.R
@@ -0,0 +1,7 @@
+## ipums_explore.R
+## Explore historical census data pulled using IPUMS
+## Requires: 
+## Last edited 8/9/22 by Tal Roded
+##########################################################
+
+library(ipumsr)
