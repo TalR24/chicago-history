33de5fba6bbbb74ddbd66a4b7b0889ea082cbcd1
Tal Roded <troded24@gmail.com>
2022-08-10 12:51:00 -0400

    check out opportunity insights zip code data

---
+++ code/explore_oi.R
@@ -0,0 +1,15 @@
+## explore_oi.R
+
+setwd("C:/Users/trode/Downloads")
+
+library(tidyverse)
+
+data <- read.csv("social_capital_zip.csv")
+
+data$zip <- as.character(data$zip)
+
+## keep if zip code starts with 606 and are in Cook county
+chicago_data <- data %>%
+  filter(substr(data$zip, 1, 3) == "606") %>%
+  filter(county==17031) %>%
+  mutate(year=2018)
